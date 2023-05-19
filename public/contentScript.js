ca9e11d09d426fbd27145d75996e61e5ffefad62
saitama1899 <eric.lorien92@gmail.com>
2023-05-19 11:26:56 +0200

    add content,  get access token and check and contitional render

--- public/contentScript.js
+++ public/contentScript.js
@@ -1,7 +1,24 @@
-let miElemento = document.getElementById('wrapper')
+/* global chrome */
 
-if (miElemento) {
-  let nuevoElemento = document.createElement('div')
-  nuevoElemento.textContent = 'Este es un nuevo elemento agregado por mi extensión de Chrome.'
-  miElemento.appendChild(nuevoElemento)
+const jobOfferRegex = /^https:\/\/www.infojobs.net\/.+\/.+\/of-.+/
+
+if (jobOfferRegex.test(window.location.href)) {
+  // La URL coincide con el patrón de una página de oferta de empleo
+
+  // Enviar un mensaje al background script para recuperar el access token
+  chrome.runtime.sendMessage({message: "getAccessToken"}, function(response) {
+    const newElement = document.createElement('div')
+    newElement.classList.add('container-expanded', 'panel-canvas', 'panel-rounded', 'inner-expanded', 'inner-s')
+    
+    // Mostrar contenido diferente dependiendo de si hay un access token o no
+    if (response.accessToken) {
+      newElement.textContent = 'Usuario logueado. Access token: ' + response.accessToken
+    } else {
+      newElement.textContent = 'Para poder utilizar Infojobs Assistant, debes iniciar sesión en Infojobs.'
+    }
+
+    // Localizar el elemento existente e insertar el nuevo elemento antes de él
+    const existingElement = document.querySelector('.container-expanded.panel-default')
+    existingElement.parentNode.insertBefore(newElement, existingElement)
+  })
 }
