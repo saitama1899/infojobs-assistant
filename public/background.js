0656e5d2b871714fe6f6d7dacabc7af1e7fdc081
saitama1899 <eric.lorien92@gmail.com>
2023-05-23 12:32:18 +0200

    add calculate expiration time, refactor and control

--- public/background.js
+++ public/background.js
@@ -5,6 +5,7 @@ const clientSecret = "DCDxBw9SLFwVP8rmJL1Td4uAEQseSMLIPeUM01b6vXR/BLYxOq";
 const redirectUri = "https://ijkeilgfehinjmckjafpllcoonflgcfh.chromiumapp.org/"
 const scope = "MY_APPLICATIONS,CANDIDATE_PROFILE_WITH_EMAIL,CANDIDATE_READ_CURRICULUM_SKILLS,CV";
 let authUrl = `https://www.infojobs.net/api/oauth/user-authorize/index.xhtml?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}`
+let isAuthenticating = false;
 
 function fetchToken(authCode) {
   const tokenUrl = 'https://www.infojobs.net/oauth/authorize'
@@ -24,11 +25,13 @@ function fetchToken(authCode) {
         return;
     }
 
-    // Now you have the access token, you can save it for later use
-    console.log('Access token:', data.access_token)
+    // Calculate the expiration time
+    const now = new Date().getTime();
+    const expiresInMilliseconds = (data.expires_in || 3600) * 1000;  // if expires_in is not present, defaulting to 1 hour
+    const expirationTime = now + expiresInMilliseconds;
 
-    // You may want to save it to the chrome local storage
-    chrome.storage.local.set({accessToken: data.access_token}, function() {
+    // Save the access token and the expiration time
+    chrome.storage.local.set({accessToken: data.access_token, expirationTime}, function() {
         if (chrome.runtime.lastError) {
             console.error('Error setting access token:', chrome.runtime.lastError)
         } else {
@@ -39,31 +42,46 @@ function fetchToken(authCode) {
   .catch(err => console.log(err))
 }
 
+function initiateAuthenticationFlow() {
+  if (isAuthenticating) {
+    // Ya está en curso la autenticación
+    return;
+  }
+
+  isAuthenticating = true;
+
+  chrome.identity.launchWebAuthFlow({
+    'url': authUrl,
+    'interactive': true
+  }, function(redirectedTo) {
+    isAuthenticating = false; // resetear la bandera cuando la autenticación ha terminado
+
+    if (chrome.runtime.lastError) {
+      if (chrome.runtime.lastError.message !== 'The user did not approve access.') {
+        console.error(chrome.runtime.lastError.message)
+      }
+    } else {
+      let url = new URL(redirectedTo)
+      let params = new URLSearchParams(url.search)
+      let authCode = params.get('code')
+      if (authCode) {
+        fetchToken(authCode)
+      } else {
+        console.error('No authorization code found in the redirected URL.')
+      }
+    }
+  })
+}
+
 chrome.webNavigation.onCommitted.addListener((details) => {
-  if (details.url.includes('infojobs.net')) {
+  if (details.url.includes('infojobs.net') && details.url !== authUrl) {
     // Inicia el flujo de autenticación cada vez que visitas 'infojobs.net'
-    chrome.storage.local.get('accessToken', function(result) {
-      if (!result.accessToken) {
-        // If no access token exists, initiate a new authentication flow
-        chrome.identity.launchWebAuthFlow({
-          'url': authUrl,
-          'interactive': true
-        }, function(redirectedTo) {
-          if (chrome.runtime.lastError) {
-            if (chrome.runtime.lastError.message !== 'The user did not approve access.') {
-              console.error(chrome.runtime.lastError.message)
-            }
-          } else {
-            let url = new URL(redirectedTo)
-            let params = new URLSearchParams(url.search)
-            let authCode = params.get('code')
-            if (authCode) {
-              fetchToken(authCode)
-            } else {
-              console.error('No authorization code found in the redirected URL.')
-            }
-          }
-        })
+    chrome.storage.local.get(['accessToken', 'expirationTime'], function(result) {
+      const now = new Date().getTime();
+
+      if (!result.accessToken || now >= result.expirationTime) {
+        // If no access token exists or if the token has expired, initiate a new authentication flow
+        initiateAuthenticationFlow();
       } else {
         console.log('Access token already exists:', result.accessToken)
       }
@@ -72,17 +90,40 @@ chrome.webNavigation.onCommitted.addListener((details) => {
 })
 
 chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  
   if (request.message === "getAccessToken") {
-    chrome.storage.local.get('accessToken', function(result) {
-      sendResponse({accessToken: result.accessToken})
+    chrome.storage.local.get(['accessToken', 'expirationTime'], function(result) {
+      const now = new Date().getTime();
+      if (!result.accessToken || now >= result.expirationTime) {
+        // Si no existe un token de acceso o si el token ha expirado, envía una respuesta indicando que el token no es válido
+        sendResponse({status: 'invalid_token'});
+      } else {
+        sendResponse({accessToken: result.accessToken})
+      }
     });
 
     // Indicar que la respuesta se enviará de forma asincrónica
     return true;
   }
+
+  if (request.message === "fetchApiData") {
+    fetch("https://ifa-api-phi.vercel.app/api/infojobs", {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ 
+            accessToken: request.accessToken, 
+            offerId: request.offerId
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        // Haz algo con los datos recibidos
+        console.log(data);
+    })
+    .catch((error) => {
+        console.error('Error:', error);
+    });
+  }
 })
-
-
-
-
-
--- public/contentScript.js
+++ public/contentScript.js
@@ -11,10 +11,32 @@ if (jobOfferRegex.test(window.location.href)) {
     newElement.classList.add('container-expanded', 'panel-canvas', 'panel-rounded', 'inner-expanded', 'inner-s')
     
     // Mostrar contenido diferente dependiendo de si hay un access token o no
-    if (response.accessToken) {
-      newElement.textContent = 'Usuario logueado. Access token: ' + response.accessToken
-    } else {
+    if (response.status === 'invalid_token') {
+      // Manejar el caso de un token no válido aquí
       newElement.textContent = 'Para poder utilizar Infojobs Assistant, debes iniciar sesión en Infojobs.'
+    } else {
+      newElement.textContent = 'Usuario logueado. Access token: ' + response.accessToken
+      
+      // Ahora buscar el id de la oferta
+      let scriptElement = [...document.querySelectorAll("script")]
+                .find(element => element.id === "tealium-data");
+
+      let scriptContent = scriptElement.textContent || scriptElement.innerText;
+      let match = scriptContent.match(/utag_data\.ad_id='(.*?)';/);
+            
+      if (match && match[1]) {
+          const offerId = match[1];
+          newElement.textContent += '. Offer id: ' + offerId;
+
+          const button = document.createElement('button');
+          button.textContent = "Consulta API";
+          button.onclick = function() {
+              chrome.runtime.sendMessage({message: "fetchApiData", accessToken: response.accessToken, offerId: offerId});
+          };
+          newElement.appendChild(button);
+      } else {
+          newElement.textContent += '. No se encontró id de oferta';
+      }
     }
 
     // Localizar el elemento existente e insertar el nuevo elemento antes de él
