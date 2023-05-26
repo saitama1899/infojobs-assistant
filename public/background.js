ae50cab72bf1640d99876b61ffa36b70f546f3eb
saitama1899 <eric.lorien92@gmail.com>
2023-05-26 11:28:38 +0200

    add analytics header and loading desings

--- public/background.js
+++ public/background.js
@@ -1,11 +1,11 @@
 /* global chrome */
 
 let clientId = '9a3461370cad412298bebf3dec098ede'
-const clientSecret = "DCDxBw9SLFwVP8rmJL1Td4uAEQseSMLIPeUM01b6vXR/BLYxOq";
+const clientSecret = "DCDxBw9SLFwVP8rmJL1Td4uAEQseSMLIPeUM01b6vXR/BLYxOq"
 const redirectUri = "https://ijkeilgfehinjmckjafpllcoonflgcfh.chromiumapp.org/"
-const scope = "MY_APPLICATIONS,CANDIDATE_PROFILE_WITH_EMAIL,CANDIDATE_READ_CURRICULUM_SKILLS,CANDIDATE_READ_CURRICULUM_CVTEXT,CANDIDATE_READ_CURRICULUM_EDUCATION,CANDIDATE_READ_CURRICULUM_EXPERIENCE,CANDIDATE_READ_CURRICULUM_FUTURE_JOB,CV";
+const scope = "MY_APPLICATIONS,CANDIDATE_PROFILE_WITH_EMAIL,CANDIDATE_READ_CURRICULUM_SKILLS,CANDIDATE_READ_CURRICULUM_CVTEXT,CANDIDATE_READ_CURRICULUM_EDUCATION,CANDIDATE_READ_CURRICULUM_EXPERIENCE,CANDIDATE_READ_CURRICULUM_FUTURE_JOB,CV"
 let authUrl = `https://www.infojobs.net/api/oauth/user-authorize/index.xhtml?client_id=${clientId}&response_type=code&redirect_uri=${encodeURIComponent(redirectUri)}&scope=${scope}`
-let isAuthenticating = false;
+let isAuthenticating = false
 
 function fetchToken(authCode) {
   const tokenUrl = 'https://www.infojobs.net/oauth/authorize'
@@ -22,22 +22,22 @@ function fetchToken(authCode) {
   .then(data => {
     if (!data.access_token) {
         console.log('Failed to obtain access token')
-        return;
+        return
     }
 
     // Calculate the expiration time
-    const now = new Date().getTime();
-    const expiresInMilliseconds = (data.expires_in || 3600) * 1000;  // if expires_in is not present, defaulting to 1 hour
-    const expirationTime = now + expiresInMilliseconds;
+    const now = new Date().getTime()
+    const expiresInMilliseconds = (data.expires_in || 3600) * 1000  // if expires_in is not present, defaulting to 1 hour
+    const expirationTime = now + expiresInMilliseconds
 
     // Save the access token and the expiration time
     chrome.storage.local.set({accessToken: data.access_token, expirationTime}, function() {
         if (chrome.runtime.lastError) {
-            console.error('Error setting access token:', chrome.runtime.lastError)
+          console.error('Error setting access token:', chrome.runtime.lastError)
         } else {
-            console.log('Token is stored in local storage')
+          console.log('Token is stored in local storage')
         }
-    });
+    })
   })
   .catch(err => console.log(err))
 }
@@ -45,16 +45,16 @@ function fetchToken(authCode) {
 function initiateAuthenticationFlow() {
   if (isAuthenticating) {
     // Ya está en curso la autenticación
-    return;
+    return
   }
 
-  isAuthenticating = true;
+  isAuthenticating = true
 
   chrome.identity.launchWebAuthFlow({
     'url': authUrl,
     'interactive': true
   }, function(redirectedTo) {
-    isAuthenticating = false; // resetear la bandera cuando la autenticación ha terminado
+    isAuthenticating = false // resetear la bandera cuando la autenticación ha terminado
 
     if (chrome.runtime.lastError) {
       if (chrome.runtime.lastError.message !== 'The user did not approve access.') {
@@ -77,11 +77,11 @@ chrome.webNavigation.onCommitted.addListener((details) => {
   if (details.url.includes('infojobs.net') && details.url !== authUrl) {
     // Inicia el flujo de autenticación cada vez que visitas 'infojobs.net'
     chrome.storage.local.get(['accessToken', 'expirationTime'], function(result) {
-      const now = new Date().getTime();
+      const now = new Date().getTime()
 
       if (!result.accessToken || now >= result.expirationTime) {
         // If no access token exists or if the token has expired, initiate a new authentication flow
-        initiateAuthenticationFlow();
+        initiateAuthenticationFlow()
       } else {
         console.log('Access token already exists:', result.accessToken)
       }
@@ -93,17 +93,17 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
   
   if (request.message === "getAccessToken") {
     chrome.storage.local.get(['accessToken', 'expirationTime'], function(result) {
-      const now = new Date().getTime();
+      const now = new Date().getTime()
       if (!result.accessToken || now >= result.expirationTime) {
         // Si no existe un token de acceso o si el token ha expirado, envía una respuesta indicando que el token no es válido
-        sendResponse({status: 'invalid_token'});
+        sendResponse({status: 'invalid_token'})
       } else {
         sendResponse({accessToken: result.accessToken})
       }
-    });
+    })
 
     // Indicar que la respuesta se enviará de forma asincrónica
-    return true;
+    return true
   }
 
   if (request.message === "fetchApiData") {
@@ -120,10 +120,10 @@ chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
     .then(response => response.json())
     .then(data => {
         // Haz algo con los datos recibidos
-        console.log(data);
+        console.log(data)
     })
     .catch((error) => {
-        console.error('Error:', error);
-    });
+        console.error('Error:', error)
+    })
   }
 })
--- public/contentScript.js
+++ public/contentScript.js
@@ -3,44 +3,71 @@
 const jobOfferRegex = /^https:\/\/www.infojobs.net\/.+\/.+\/of-.+/
 
 if (jobOfferRegex.test(window.location.href)) {
-  // La URL coincide con el patrón de una página de oferta de empleo
+  document.getElementById('howyoumatchcard').style.display = 'none'
 
-  // Enviar un mensaje al background script para recuperar el access token
   chrome.runtime.sendMessage({message: "getAccessToken"}, function(response) {
-    const newElement = document.createElement('div')
-    newElement.classList.add('container-expanded', 'panel-canvas', 'panel-rounded', 'inner-expanded', 'inner-s')
-    
-    // Mostrar contenido diferente dependiendo de si hay un access token o no
-    if (response.status === 'invalid_token') {
-      // Manejar el caso de un token no válido aquí
-      newElement.textContent = 'Para poder utilizar Infojobs Assistant, debes iniciar sesión en Infojobs.'
-    } else {
-      newElement.textContent = 'Usuario logueado. Access token: ' + response.accessToken
-      
-      // Ahora buscar el id de la oferta
-      let scriptElement = [...document.querySelectorAll("script")]
-                .find(element => element.id === "tealium-data");
+    const ijaAnalytics = document.createElement('section')
+    ijaAnalytics.classList.add('container-expanded', 'panel-canvas', 'panel-rounded', 'inner-expanded', 'inner')
+    const content = document.createElement('div')
+    content.style.display = 'flex'
+    content.style.alignItems = 'start'
+    content.style.gap = '20px'
+    ijaAnalytics.appendChild(content)
 
-      let scriptContent = scriptElement.textContent || scriptElement.innerText;
-      let match = scriptContent.match(/utag_data\.ad_id='(.*?)';/);
+    const logo = document.createElement('img')
+    logo.src = 'https://res.cloudinary.com/dykoj2iuj/image/upload/v1685080130/logo_xcc4nz.png'
+    logo.style.width = '40px'
+    content.appendChild(logo)
+
+    if (response.status === 'invalid_token') {
+      let title = document.createElement('h3')
+      title.textContent = 'Para poder utilizar Infojobs Assistant, debes iniciar sesión en Infojobs'
+      content.appendChild(title)
+    } else {
+      let scriptElement = [...document.querySelectorAll("script")]
+        .find(element => element.id === "tealium-data")
+      let scriptContent = scriptElement.textContent || scriptElement.innerText
+      let match = scriptContent.match(/utag_data\.ad_id='(.*?)'/)
             
       if (match && match[1]) {
-          const offerId = match[1];
-          newElement.textContent += '. Offer id: ' + offerId;
+          const offerId = match[1]
+          console.log('Offer id: ' + offerId)
 
-          const button = document.createElement('button');
-          button.textContent = "Consulta API";
+          const rightColumn = document.createElement('div')
+          rightColumn.style.flexGrow = '1'
+          content.appendChild(rightColumn)
+          const title = document.createElement('h3')
+          title.textContent = 'Saca lo mejor de ti y consigue este empleo con la ayuda de Infojobs Assistant'
+          rightColumn.appendChild(title)
+
+          const subtitle = document.createElement('p')
+          subtitle.textContent = 'Averigua cómo puedes enfocar tu currículum para destacar entre los demás candidatos, qué puedes esperar de este proceso de seleccion, tu nivel de compatibilidad y porcentaje de éxito.'
+          rightColumn.appendChild(subtitle)
+
+          const button = document.createElement('button')
+          button.classList.add('ij-Link', 'sui-AtomButton', 'sui-AtomButton--primary', 'sui-AtomButton--outline', 'sui-AtomButton--center', 'sui-AtomButton--small', 'sui-AtomButton--link', 'no-printable')
+          button.textContent = "Analizar esta oferta"
+          rightColumn.appendChild(button)
           button.onclick = function() {
-              chrome.runtime.sendMessage({message: "fetchApiData", accessToken: response.accessToken, offerId: offerId});
-          };
-          newElement.appendChild(button);
+            // chrome.runtime.sendMessage({message: "fetchApiData", accessToken: response.accessToken, offerId: offerId})
+            rightColumn.removeChild(button)
+            subtitle.textContent = 'Por favor ten paciencia, este análisis puede tardar alrededor de 1 minuto.'
+            const loading = document.createElement('div')
+            loading.style.textAlign = 'center'
+            const gif = document.createElement('img')
+            gif.src = 'https://res.cloudinary.com/dykoj2iuj/image/upload/v1685091115/loading_ysz2da.gif'
+            gif.style.width = '80px'
+            loading.appendChild(gif)
+            rightColumn.append(loading)
+          }
+          
+
       } else {
-          newElement.textContent += '. No se encontró id de oferta';
+          console.error('No se encontró id de oferta')
       }
     }
 
-    // Localizar el elemento existente e insertar el nuevo elemento antes de él
     const existingElement = document.querySelector('.container-expanded.panel-default')
-    existingElement.parentNode.insertBefore(newElement, existingElement)
+    existingElement.parentNode.insertBefore(ijaAnalytics, existingElement)
   })
 }
